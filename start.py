ea1a1291b0b461b82c192dc9686508d65ff74f90
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-06-05 20:01:29 +0530

    Add here your grp username @sensieOfficial

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
 system ("git clone https://github.com/SenseiOfficial/Sensei-BOT && cd userbot && python3 -m userbot")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO ")
