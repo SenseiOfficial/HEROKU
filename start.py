8da1276655a969afb16d26b981cb018ea9de4da3
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2021-06-06 14:16:27 +0530

    😁😁

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
 system ("git clone https://github.com/SenseiOfficial/Sensei-BOT && cd Sensei-BOT && python3 -m userbot")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO ")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @KINGBOTOFFICIAL")
